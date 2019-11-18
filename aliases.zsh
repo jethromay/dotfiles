60df5751cd3ece4c0c5aa410ef281fe442029074
Jethro May <jethro@plusnarrative.com>
2019-11-18 10:45:03 +0200

    Added custom settings

--- aliases.zsh
+++ aliases.zsh
@@ -1,13 +1,6 @@
 # Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
-alias reloadcli="source $HOME/.zshrc"
-alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -ahlF --color --group-directories-first"
-weather() { curl -4 wttr.in/${1:-antwerp} }
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
-alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias pstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias c="clear"
-alias zbundle="antibody bundle < $DOTFILES/zsh_plugins.txt > $DOTFILES/zsh_plugins.sh"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
@@ -16,8 +9,7 @@ alias sites="cd $HOME/Sites"
 alias lara="sites && cd laravel/"
 
 # Laravel
-alias a="php artisan"
-alias ams="php artisan migrate:fresh --seed"
+alias art="php artisan"
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
@@ -34,24 +26,21 @@ alias vssh="vagrant ssh"
 alias vreload="vagrant reload"
 alias vrebuild="vagrant destroy --force && vagrant up"
 
+# Homestead
+alias hs="Homestead"
+
 # Docker
-alias docker-composer="docker-compose"
-#alias dstop="docker stop $(docker ps -a -q)"
-#alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
-#alias dpurgeimages="docker rmi $(docker images -q)"
-#dbuild() { docker build -t=$1 .; }
-#dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+alias dup="docker-compose up -d"
+alias ddown="docker-compose down"
 
 # Git
 alias commit="git add . && git commit -m"
-alias gcommit="git add . && git commit"
-alias amend="git commit --amend --no-edit"
-alias amendall="git add . && amend"
 alias wip="commit wip"
 alias gst="git status"
+alias gpull="git pull"
+alias gpush="git push"
 alias gb="git branch"
 alias gc="git checkout"
 alias gd="git diff"
-alias resolve="git add . && git commit --no-edit"
-alias gl="git log --oneline --decorate --color"
-alias nuke="git clean -df && git reset --hard"
+alias glog="git log --oneline --decorate --color"
+alias nah="git clean -df && git reset --hard"
