19d578d1bd6ee6d85c21e6e3de8a35c9f66cb1b8
Jethro May <hello@jethromay.com>
2020-09-22 09:07:53 +0200

    Updated aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -6,7 +6,6 @@ alias c="clear"
 alias dotfiles="cd $DOTFILES"
 alias library="cd $HOME/Library"
 alias sites="cd $HOME/Sites"
-alias lara="sites && cd laravel/"
 
 # Laravel
 alias art="php artisan"
